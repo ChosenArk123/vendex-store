837dfdca2f92d58c67485ae9a6d0ffdcf3099029
Anish Paruchuri <aniparuc@anishs-air.lan>
2025-12-14 00:10:07 -0600

    Fixed Search Function, revamped server file

--- server.js
+++ server.js
@@ -1,32 +1,56 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/Product'); // <--- This was missing!
+
 const app = express();
-const fs = require('fs');
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
-// THE HOMEPAGE ROUTE
-app.get('/', (req, res) => {
-    // 1. Read the products from our JSON file
-    const productsData = fs.readFileSync('products.json');
-    const products = JSON.parse(productsData);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-    // 2. Render the 'index' template and send the products data to it
-    res.render('index', { products: products });
+// THE HOMEPAGE ROUTE (With Search)
+app.get('/', async (req, res) => {
+    // 1. Check if there is a search query in the URL
+    const searchQuery = req.query.search;
+    let mongoQuery = {};
+
+    // 2. If user searched, build a database filter
+    if (searchQuery) {
+        mongoQuery = {
+            $or: [
+                { title: { $regex: searchQuery, $options: 'i' } },
+                { description: { $regex: searchQuery, $options: 'i' } },
+                { brand: { $regex: searchQuery, $options: 'i' } }
+            ]
+        };
+    }
+
+    // 3. Get products from MongoDB
+    const products = await Product.find(mongoQuery);
+    
+    // 4. Render the page
+    res.render('index', { 
+        products: products, 
+        searchQuery: searchQuery || '' 
+    });
 });
 
-// NEW: Product Detail Route
-app.get('/product/:id', (req, res) => {
-    const productsData = fs.readFileSync('products.json');
-    const products = JSON.parse(productsData);
+// PRODUCT DETAIL ROUTE (Updated to use MongoDB)
+app.get('/product/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
     
-    // Find the specific product by ID
-    const product = products.find(p => p.id == req.params.id);
+    // Find product in MongoDB
+    const product = await Product.findOne({ id: id });
     
     if (product) {
         res.render('product', { product: product });
     } else {
-        res.send('Product not found');
+        res.status(404).send('Product not found');
     }
 });
 
